ede098e3bc965ee644e7442fb921b7ddc28a3a7a
Strider Wilson <Strider@Striders-MBP.T-mobile.com>
2019-10-10 17:04:05 -0700

    Completes stringifyJSON using recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,31 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var strArr = '[';
+  var newArr = [];
+  if (typeof obj === 'number') {
+    return '' + obj;;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return "" + null;
+  } else if (typeof obj === 'boolean') {
+    return "" + obj
+  } if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      newArr.push(stringifyJSON(obj[i]));
+    }
+    return '[' + `${newArr}` + ']'
+  } else if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        return '{}'
+      }
+      newArr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + `${newArr}` + '}'
+  }
 };
